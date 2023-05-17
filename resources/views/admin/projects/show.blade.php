0a3f90b4200244c4b3f72c50772515cf4a442188
daniloeberli <danilo.eberli@gmail.com>
2023-05-17 15:15:26 +0200

    fix: show view title

--- resources/views/admin/projects/show.blade.php
+++ resources/views/admin/projects/show.blade.php
@@ -3,12 +3,11 @@
 @section('content')
 <div class="container">
     <h2 class="fs-4 text-secondary my-4">
-        Lista progetti
+        {{$project->title}}
     </h2>
 </div>
 <div class="container">
     <ul>
-        <li>{{$project->title}}</li>
         <li>{{$project->stack}}</li>
         <li>{{$project->description}}</li>
     </ul>
